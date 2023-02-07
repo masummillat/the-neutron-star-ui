60948021a6804656cf985b393fab6e0066a77698
Masum Millat <maummillat@gmail.com>
2023-02-07 20:25:44 +0600

    storybook build

---
+++ storybook-static/assets/Button.stories-b25b8d96.js
@@ -0,0 +1,47 @@
+import{B as q}from"./index-2c48a072.js";import"./jsx-runtime-670450c2.js";import"./index-f1f749bf.js";import"./_commonjsHelpers-042e6b4d.js";const F={title:"Example/Button",component:q,tags:["autodocs"],argTypes:{}},e={args:{label:"Button"}},r={args:{label:"Button"}},a={args:{size:"large",label:"Button"}},s={args:{size:"small",label:"Button",buttonType:"outlined"}},t={args:{size:"medium",label:"Outlined",buttonType:"outlined"}},o={args:{size:"medium",label:"Text",buttonType:"text"}},n={args:{size:"medium",label:"Outlined",buttonType:"outlined",disabled:!0,icon:"+"}},l={args:{label:"Loading",loading:!0,icon:"+"}};var c,u,i;e.parameters={...e.parameters,docs:{...(c=e.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    label: 'Button'
+  }
+}`,...(i=(u=e.parameters)==null?void 0:u.docs)==null?void 0:i.source}}};var d,m,p;r.parameters={...r.parameters,docs:{...(d=r.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    label: 'Button'
+  }
+}`,...(p=(m=r.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};var g,b,y;a.parameters={...a.parameters,docs:{...(g=a.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    size: 'large',
+    label: 'Button'
+  }
+}`,...(y=(b=a.parameters)==null?void 0:b.docs)==null?void 0:y.source}}};var T,S,B;s.parameters={...s.parameters,docs:{...(T=s.parameters)==null?void 0:T.docs,source:{originalSource:`{
+  args: {
+    size: 'small',
+    label: 'Button',
+    buttonType: 'outlined'
+  }
+}`,...(B=(S=s.parameters)==null?void 0:S.docs)==null?void 0:B.source}}};var z,x,O;t.parameters={...t.parameters,docs:{...(z=t.parameters)==null?void 0:z.docs,source:{originalSource:`{
+  args: {
+    size: 'medium',
+    label: 'Outlined',
+    buttonType: 'outlined'
+  }
+}`,...(O=(x=t.parameters)==null?void 0:x.docs)==null?void 0:O.source}}};var L,f,D;o.parameters={...o.parameters,docs:{...(L=o.parameters)==null?void 0:L.docs,source:{originalSource:`{
+  args: {
+    size: 'medium',
+    label: 'Text',
+    buttonType: 'text'
+  }
+}`,...(D=(f=o.parameters)==null?void 0:f.docs)==null?void 0:D.source}}};var E,P,_;n.parameters={...n.parameters,docs:{...(E=n.parameters)==null?void 0:E.docs,source:{originalSource:`{
+  args: {
+    size: 'medium',
+    label: 'Outlined',
+    buttonType: 'outlined',
+    disabled: true,
+    icon: '+'
+  }
+}`,...(_=(P=n.parameters)==null?void 0:P.docs)==null?void 0:_.source}}};var h,j,k;l.parameters={...l.parameters,docs:{...(h=l.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  args: {
+    label: 'Loading',
+    loading: true,
+    icon: '+'
+  }
+}`,...(k=(j=l.parameters)==null?void 0:j.docs)==null?void 0:k.source}}};const G=["Primary","Secondary","Large","Small","Outlined","Text","Disabled","Loading"];export{n as Disabled,a as Large,l as Loading,t as Outlined,e as Primary,r as Secondary,s as Small,o as Text,G as __namedExportsOrder,F as default};
+//# sourceMappingURL=Button.stories-b25b8d96.js.map
