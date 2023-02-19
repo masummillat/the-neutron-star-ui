2a1270eda89032960d9da22a8766c23319032fa6
Masum Millat <maummillat@gmail.com>
2023-02-19 21:14:02 +0600

    typography update

---
+++ src/components/Avatar/Avatar.tsx
@@ -0,0 +1,27 @@
+import clsx from 'clsx';
+import React from 'react';
+import { AvatarProps } from './Avatar.types';
+const defaultImg =
+  'https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80';
+const Avatar: React.FC<AvatarProps> = ({
+  size = 'md',
+  alt,
+  src = defaultImg
+}) => {
+  const sizes = new Map([
+    ['sm', 'h-8 w-8'],
+    ['md', 'h-12 w-12'],
+    ['lg', 'h-14 w-14']
+  ]);
+  return (
+    <img
+      className={clsx(
+        'inline-block rounded-full ring-2 ring-white',
+        sizes.get(size)
+      )}
+      src={src}
+      alt={alt || ''}
+    />
+  );
+};
+export default Avatar;
---
+++ src/components/Avatar/Avatar.types.ts
@@ -0,0 +1,5 @@
+export interface AvatarProps{
+    src?: string;
+    size?: "sm" | "md" | "lg"
+    alt?: string;
+}
